a8e0b88cf4f905e363d5bd6797f21e6e92e00fb8
nirenrawal <nirenrawal@gmail.com>
2022-05-28 00:05:48 +0200

    serving added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
-
 document.querySelector("#menubutton").addEventListener("click", openMenu);
 
+function openMenu() {
+  document.querySelector("#menubutton").classList.toggle("fa-bars");
+  document.querySelector("#menubutton").classList.toggle("fa-x");
+  document.querySelector("#menubutton").classList.toggle("menubutton_color");
+  document.querySelector(".menu_items").classList.toggle("menu_items_position");
+  document.querySelector(".menu_items").classList.toggle("hidden");
+}
 
-function openMenu(){
-    document.querySelector("#menubutton").classList.toggle("fa-bars");
-    document.querySelector("#menubutton").classList.toggle("fa-x");
-    document.querySelector("#menubutton").classList.toggle("menubutton_color");
-    document.querySelector(".menu_items").classList.toggle("menu_items_position");
-    document.querySelector(".menu_items").classList.toggle("hidden");
-};
-
+let data = 1;
+document.getElementById("counting").innerText = data;
+function increase() {
+  data = data + 1;
+  document.getElementById("counting").innerText = data;
+}
+function decrease() {
+  data = Math.max(1, data - 1);
+  document.getElementById("counting").innerText = data;
+}
