270c4f18694e8efe537bc12f4f8c27236a9da920
nirenrawal <nirenrawal@gmail.com>
2022-05-28 12:52:13 +0200

    weight conversion and nr of servings done

--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
 document.querySelector("#menubutton").addEventListener("click", openMenu);
 
 function openMenu() {
-  document.querySelector("#menubutton").classList.toggle("fa-bars");
-  document.querySelector("#menubutton").classList.toggle("fa-x");
-  document.querySelector("#menubutton").classList.toggle("menubutton_color");
-  document.querySelector(".menu_items").classList.toggle("menu_items_position");
-  document.querySelector(".menu_items").classList.toggle("hidden");
+    document.querySelector("#menubutton").classList.toggle("fa-bars");
+    document.querySelector("#menubutton").classList.toggle("fa-x");
+    document.querySelector("#menubutton").classList.toggle("menubutton_color");
+    document.querySelector(".menu_items").classList.toggle("menu_items_position");
+    document.querySelector(".menu_items").classList.toggle("hidden");
 }
 
+
+
+//############### COUNTER ###################//
 let data = 1;
 document.getElementById("counting").innerText = data;
 function increase() {
-  data = data + 1;
-  document.getElementById("counting").innerText = data;
+    data = data + 1;
+    document.getElementById("counting").innerText = data;
 }
 function decrease() {
-  data = Math.max(1, data - 1);
-  document.getElementById("counting").innerText = data;
+    data = Math.max(1, data - 1);
+    document.getElementById("counting").innerText = data;
 }
+
+//############### WEIGHT CONVERSION ###################//
+let gramBtn = document.getElementById('gram');
+let ounceBtn = document.getElementById('ounce');
+
+gramBtn.addEventListener('click', function () {
+    let input = document.getElementById('input').value;
+    document.getElementById('output').value = input / 28.34952;
+
+});
+
+ounceBtn.addEventListener('click', function () {
+    let input = document.getElementById('input').value;
+    document.getElementById('output').value = input * 28.34952;
+
+});
